e6a4772d0740415353bc78bd19dd115ab5e4f00e
if164ever1 <acere5ihor@gmail.com>
2023-06-26 15:02:57 +0200

    create Singleton class

---
+++ Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+public class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObject = new object();
+
+    private Logger()
+    {
+        // Private constructor to prevent instantiation from outside the class.
+    }
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void Log(string message)
+    {
+        // Code to log the message goes here.
+        Console.WriteLine($"Logging message: {message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Logger logger = Logger.Instance;
+        logger.Log("This is a log message.");
+    }
+}
+
